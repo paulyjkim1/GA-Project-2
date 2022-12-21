d6e4be5ad67066f43ba7f18f92da11d71a599a69
paulyjkim1 <paulyjkim1@gmail.com>
2022-12-21 13:42:18 -0800

    finishes part 2, login and logout added

---
+++ controllers/users.js
@@ -0,0 +1,81 @@
+//create an instance of express routers
+const express = require('express')
+const { RowDescriptionMessage } = require('pg-protocol/dist/messages')
+const db = require('../models')
+const router = express.Router()
+
+//mount our routes on the router
+
+// GET /users/new -- serves a form to create a new user
+router.get('/new', (req, res) => {
+    res.render('users/new.ejs')
+})
+
+// POST /users -- creates a new user from the form @ /users/new
+router.post('/', async (req, res) => {
+    try {
+        //based on the info in the req.body, find or create user
+        const [newUser, created] = await db.user.findOrCreate({
+            where: {
+                email: req.body.email
+            },
+            //TODO: dont add plaintext passwords to the db
+            defaults: {
+                password: req.body.password
+            }
+        })
+        //TODO: redirect to the login page if the user is found
+        //log the user in (store the usesr's id as a cookie in the browser)
+        res.cookie('userId', newUser.id)
+        //redirect to home page (for now)
+        res.redirect('/')
+        
+    } catch (err) {
+        console.log(err)
+        res.status(500).send('server error')
+    }
+})
+
+router.get('/login', (req, res)=> {
+    res.render('users/login.ejs', {
+        message: req.query.message ? req.query.message : null
+    })
+})
+//ingest data from form rendered @ get /users/login
+router.post('/login', async (req, res) => {
+    try{
+        // look up the user based on their email
+        const user = await db.user.findOne({
+            where: {
+                email: req.body.email
+            }
+        })
+        //boilerplate message if login fais
+        const badCredentialMessage = 'username or password incorrect'
+        if (!user) {
+            // if the user isn't found in the db 
+            res.redirect('/users/login?message=' + badCredentialMessage)
+        } else if (user.password !== req.body.password) {
+            // if the user's supplied password is incorrect
+            res.redirect('/users/login?message=' + badCredentialMessage)
+        } else {
+            // if the user is found and their password matches-log them in
+            console.log('loggin user in')
+            res.cookie('userId', user.id)
+            res.redirect('/')
+        }
+    }catch (err) {
+        console.log(err)
+        res.status(500).send('server error')
+    }
+})
+
+router.get('/logout', (req, res) => {
+    // log the user out by removing the cookie
+    // make a get req to /
+    res.clearCookie('userId')
+    res.redirect('/')
+})
+
+//export the router 
+module.exports = router
--- index.js
+++ index.js
@@ -6,12 +6,16 @@ const express = require('express')
 const app = express()
 const PORT = process.env.PORT || 8000
 app.set('viewengine', 'ejs')
+// parse request bodies from the html forms
+app.use(express.urlencoded({ extended: false }))
 
 
 //routes and controllers
 app.get('/', (req, res) => {
     res.render('home.ejs')
 })
+
+app.use('/users', require('./controllers/users'))
 
 app.listen(PORT, () => {
     console.log(`authenticating on PORT ${PORT}`)
